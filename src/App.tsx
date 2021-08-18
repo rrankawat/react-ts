288249979ef9e76c758f6f79d50c9b001e224063
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-08-18 23:22:22 +0530

    Unions in State

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import GuestList from './state/GuestList'
+import UserSearch from './state/UserSearch'
 
 const App = () => {
   return (
     <div>
-      <GuestList />
+      <UserSearch />
     </div>
   )
 }
